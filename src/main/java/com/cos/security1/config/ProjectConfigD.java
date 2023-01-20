97ae156a52637f1c83a2429cd38ccac1e4011fe9
h0l1da2 <whaleee@naver.com>
2023-01-20 23:48:21 +0900

    httpBasic 설정을 통해 영역 이름 등 인증 방식과 관련된 일부 구성 변경 가능함!

--- src/main/java/com/cos/security1/config/ProjectConfigD.java
+++ src/main/java/com/cos/security1/config/ProjectConfigD.java
@@ -1,35 +1,35 @@
-package com.cos.security1.config;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-@EnableAsync // 보통은 책임을 분리하기때문에, 절대 주 클래스를 구성 클래스로 이용하지 않음
-@Configuration
-public class ProjectConfigD {
-
-    /**
-     * 프레임워크가 자체적으로 스레드를 만들 때만 작동함(ex: @Async 메서드 이용)
-     * 코드로 스레드를 만들면 이 설정가지고는 안 됨
-     */
+//package com.cos.security1.config;
+//
+//import org.springframework.beans.factory.InitializingBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//
+//@EnableAsync // 보통은 책임을 분리하기때문에, 절대 주 클래스를 구성 클래스로 이용하지 않음
+//@Configuration
+//public class ProjectConfigD {
+//
+//    /**
+//     * 프레임워크가 자체적으로 스레드를 만들 때만 작동함(ex: @Async 메서드 이용)
+//     * 코드로 스레드를 만들면 이 설정가지고는 안 됨
+//     */
+////    @Bean
+////    public InitializingBean initializingBean() {
+////        return () -> SecurityContextHolder.setStrategyName(
+////                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL
+////        );
+////    }
+//
+//    /**
+//     * 모든 쓰레드가 같은 보안 컨텍스트에 접근
+//     * 웹서버는 안 쓰고 독립된 애플리케이션에선 쓸수있음
+//     */
 //    @Bean
 //    public InitializingBean initializingBean() {
 //        return () -> SecurityContextHolder.setStrategyName(
-//                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL
+//                SecurityContextHolder.MODE_GLOBAL
 //        );
 //    }
-
-    /**
-     * 모든 쓰레드가 같은 보안 컨텍스트에 접근
-     * 웹서버는 안 쓰고 독립된 애플리케이션에선 쓸수있음
-     */
-    @Bean
-    public InitializingBean initializingBean() {
-        return () -> SecurityContextHolder.setStrategyName(
-                SecurityContextHolder.MODE_GLOBAL
-        );
-    }
-
-}
+//
+//}
---
+++ src/main/java/com/cos/security1/config/ProjectConfigE.java
@@ -0,0 +1,38 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectConfigE extends WebSecurityConfigurerAdapter {
+
+    /**
+     * httpBasic 에 매개변수를 통해서
+     * 인증 방식과 관련된 일부 구성
+     * (여기서는 영역 이름)
+     * 바꿀 수 있다는데
+     * 이해못하겠엉...
+     * - 영역 이란?
+     * 특정 인정 방식을 이용하는 보호 공간이래요(잘모르겠음)
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /**
+         * 람다 뜻
+         * Customizer<HttpBasicConfigurer<HttpSecurity>> 형식의 객체임
+         * HttpBasicConfigurer<HttpSecurity> 형식의 매개 변수로
+         * realName()을 호출해
+         * 영역 이름을 변경할 수 있게 함
+         * -
+         * HTTP 응답에 영역 이름이 변경됐는지 나옴
+         * 근데 www-authenticate 헤더에는
+         * 401 예외일때만 있고 200일때는 걍 cURL 호출함
+         */
+        http.httpBasic(c -> {
+            c.realmName("OTHER");
+        });
+
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
