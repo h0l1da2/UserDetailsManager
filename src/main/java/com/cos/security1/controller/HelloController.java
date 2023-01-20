09e717f945a5e1c4cd31cefb514f648e30e8697a
h0l1da2 <whaleee@naver.com>
2023-01-20 17:34:13 +0900

    인증 후에는 Controller 에서 시큐리티컨텍스트에 있는 본인의 authentication 을 들고올 수 있다! 거기에는 본인 세부 정보가 들어있슴다
    -
    컨텍스트를 가져와서 어센티케이션을 불러와도 되지만
    매개변수로 바로 주입받을 수도 있으니까
    그렇ㄱㅔ 쓰면 편함

--- src/main/java/com/cos/security1/controller/HelloController.java
+++ src/main/java/com/cos/security1/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.cos.security1.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello!";
+    /**
+     * 인증이 완료된 사용자 정보를
+     * 시큐리티 컨텍스트에 있는
+     * 어센티케이션에서 가져올 수 있어요
+     */
+    // SecurityContext 를 가져와서 직접 어센티케이션을 가져와서 정보 보기
+    @GetMapping("/hello1")
+    public String hello1() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        return "hello! "+authentication.getName();
+    }
+    // ** 추천
+    // 스프링 인증은 곧바로 매개변수에 주입이 가능!
+    // 그냥 매개변수로 받은 Authentication 을 바로 참조해도 됩니다
+    @GetMapping("/hello2")
+    public String hello2(Authentication authentication) {
+        return "hello! "+authentication.getName();
     }
 }
